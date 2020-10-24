c2139968d8926b69a5023b63d8fb7bcc49e463c7
xLexip <lexip.ctrl@gmail.com>
2020-10-24 16:40:49 +0200

    Implement scheduling (+user prompt)

--- clean.py
+++ clean.py
@@ -1,57 +1,54 @@
-import os, glob, time, getpass
+import os, glob, time, getpass, pathlib, subprocess
 from configparser import ConfigParser
 from send2trash import send2trash
 from datetime import datetime
 from stat import *
 
 clear = lambda: os.system('clear')
-clear();
+clear()
+print("\n>_  downbloats cleaning")
 
-print("\n >_  downbloats cleaning")
 
-# CONFIG
-#Read config.ini file
-if os.path.isfile("config.ini"):
+# READ CONFIG
+if os.path.isfile(str(pathlib.Path(__file__).parent.absolute())+"/config.ini"):
     config_object = ConfigParser()
-    config_object.read("config.ini")
+    config_object.read(str(pathlib.Path(__file__).parent.absolute())+"/config.ini")
 else:
     print("\n[!] Config file not found, running main.py...")
     input("\nPress Enter to continue...")
     clear()
-    os.system('python3 main.py')
+    os.system('python3 '+str(pathlib.Path(__file__).parent.absolute())+'/main.py')
     exit()
 
-#Get file timeout
-userinfo = config_object["CONFIG"]
-trashAccessedAfter = userinfo["trashAfter"]
+info = config_object["CONFIG"]
+trashAccessedAfter = info["trashAfter"]
 
-# GET PATH
-username = getpass.getuser()
-path = "/home/"+username+"/Downloads/*"
+
+# GET DOWNLAODS PATH
+path = "/home/"+getpass.getuser()+"/Downloads/*"
+
 
 # SCAN PATH
 files = []
 for file in glob.glob(path):
     files.append(file)
-print("\n[i]  Found "+str(len(files))+" Files in Downloads...")
+print("\n[i]  Scanning "+str(len(files))+" Files in Downloads...")
+
 
 # CLEAN PATH
-cleanedSth = 0
+cleanedSth = False
 for file in files:
     try:
         st = os.stat(file)
     except IOError:
         print("[!]  Cannot get information about :", file.rsplit('/', 1)[-1])
     else:
-        if float(time.mktime(time.localtime()) - st[ST_ATIME]) >= float(trashAccessedAfter):
+        if (int(time.mktime(time.localtime())) - int(st[ST_ATIME])) >= float(trashAccessedAfter):
             send2trash(file)
             print("[i]  Trashed: "+file.rsplit('/', 1)[-1])
-            cleanedSth = 1
+            cleanedSth = True
 
-if cleanedSth == 0:
-    print("[i] Nothing to clean.")
-
-input("\nPress Enter to continue...")
-
-
-        
+if not cleanedSth:
+    print("[✓] Nothing to clean.\n")
+else:
+    print("[✓] Cleaning completed")
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import os, sys, time, getpass, pathlib, subprocess
 from configparser import ConfigParser
-import os
 
 clear = lambda: os.system('clear')
+
+# CHECK FOR SUDO RIGHTS
+if os.geteuid() == 0:
+    print("[✗]  Do not run this with sudo privileges! Running without sudo...")
+    subprocess.call(['python3', *sys.argv])
+    sys.exit()
+else:
+    clear()
+    print("\n >_  downbloats configuration\n")
 
 #CREATE CONFIG
 config_object = ConfigParser()
@@ -14,12 +23,14 @@ with open('config.ini', 'w') as conf:
     config_object.write(conf)
 
 
-# CONFIG
+# CONFIG PROMPT
 sel = "100"
-while int(sel) > 5 or int(sel) < 0:
-    clear()
-    print("\n >_  downbloats configuration")
-    sel = input("\n[STEP 1] The clean-up process trashes all files, that have not been accessed for:\n [1] One hour\n [2] Three hours\n [3] One day\n [4] Three days\n [5] One week\n\n Your choice? (1/2/3/4/5) ")
+while int(sel) > 5 or int(sel) < 1:
+    sel = input("\n[?] The clean-up process should trash all files, that have not been accessed for at least:\n [1] One hour\n [2] Three hours\n [3] One day\n [4] Three days\n [5] One week\n\n Your choice? (1/2/3/4/5) ")
+    try: 
+        int(sel)
+    except ValueError as e:
+        sel = "100"
 
 clear()
 print("\n >_  downbloats configuration")
@@ -27,33 +38,60 @@ print("\n >_  downbloats configuration")
 config_object = ConfigParser()
 config_object.read("config.ini")
 userinfo = config_object["CONFIG"]
+choice1 = ""
 
 if int(sel) == 1:
-    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for one hour.\n")
+    choice1 = "\n[✓]  The clean-up process will trash all files, that have not been accessed for one hour.\n"
     userinfo["trashAfter"] = "3600"
 elif int(sel) == 2:
-    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for three hours.\n")
+    choice1 = "\n[✓]  The clean-up process will trash all files, that have not been accessed for three hours.\n"
     userinfo["trashAfter"] = "10800"
 elif int(sel) == 3:
-    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for one day.\n")
+    choice1 = "\n[✓]  The clean-up process will trash all files, that have not been accessed for one day.\n"
     userinfo["trashAfter"] = "86400"
 elif int(sel) == 4:
-    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for three days.\n")
+    choice1 = "\n[✓]  The clean-up process will trash all files, that have not been accessed for three days.\n"
     userinfo["trashAfter"] = "259200"
 elif int(sel) == 5:
-    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for one week.\n")
+    choice1 = "\n[✓]  The clean-up process will trash all files, that have not been accessed for one week."
     userinfo["trashAfter"] = "604800"
+
+print(choice1)
 
 with open('config.ini', 'w') as conf:
     config_object.write(conf)
 
 
-# CLEANUP sel
-sel = ""
-while sel.lower() != "y" and sel.lower() != "n":
-    sel = input("[?]  Do you want to run the ~/Downlaods/ clean-up? (Y/n)")
+# SCHEDULING PROMPT
+sel = 100;
+while int(sel) > 3 or int(sel) < 1:
+    sel = input("[?] Select how you want to schedule the clean-up:\n[1] Not at all\n[2] On system start-up\n[3] On system start-up and every hour\n\nYour choice? (1/2/3)")
+    try: 
+        int(sel)
+    except ValueError as e:
+        sel = "100"
 
-if sel.lower() == "y":
-    clear()
-    os.system('python3 clean.py')
+clear()
+print("\n >_  your downbloats configuration")
+print(choice1)
 
+if sel.lower() == "1":
+    os.system('crontab -u '+getpass.getuser()+' -l | grep -v "@reboot python3 '+str(pathlib.Path().absolute())+'/clean.py &" | crontab -u '+getpass.getuser()+' -')
+    os.system('crontab -u '+getpass.getuser()+' -l | grep -v "python3 '+str(pathlib.Path().absolute())+'/clean.py" | crontab -u '+getpass.getuser()+' -')
+    print("[✓]  The clean-up won't be run automatically.\n")
+
+elif sel.lower() == "2":
+    os.system('crontab -u '+getpass.getuser()+' -l | grep -v "python3 '+str(pathlib.Path().absolute())+'/clean.py" | crontab -u '+getpass.getuser()+' -')
+    os.system('(crontab -l ; echo "@reboot python3 '+str(pathlib.Path().absolute())+'/clean.py &") | sort - | uniq - | crontab -')
+    print("[✓]  The clean-up will be run on start-up.\n")
+
+elif sel.lower() == "3":
+    os.system('(crontab -l ; echo "@reboot python3 '+str(pathlib.Path().absolute())+'/clean.py &") | sort - | uniq - | crontab -')
+    os.system('(crontab -l ; echo "30 * * * * python3 '+str(pathlib.Path().absolute())+'/clean.py") | sort - | uniq - | crontab -')
+    print("[✓]  The clean-up will be run on start-up and every hour at half past.\n")
+
+input("\nPress Enter to continue...")
+
+
+# RUN CLEANUP
+os.system('python3 clean.py')
