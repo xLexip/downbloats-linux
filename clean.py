6d79aa10281e93340afe2f6dd3fef25bcc5dfa85
xLexip <lexip.ctrl@gmail.com>
2020-10-24 11:58:59 +0200

    Implement configuration (+user prompt)

--- clean.py
+++ clean.py
@@ -1,13 +1,29 @@
 import os, glob, time, getpass
+from configparser import ConfigParser
 from send2trash import send2trash
 from datetime import datetime
 from stat import *
-###
+
+clear = lambda: os.system('clear')
+clear();
 
 print("\n >_  downbloats cleaning")
 
 # CONFIG
-trashAccessedAfter = 172800 #TwoDays
+#Read config.ini file
+if os.path.isfile("config.ini"):
+    config_object = ConfigParser()
+    config_object.read("config.ini")
+else:
+    print("\n[!] Config file not found, running main.py...")
+    input("\nPress Enter to continue...")
+    clear()
+    os.system('python3 main.py')
+    exit()
+
+#Get file timeout
+userinfo = config_object["CONFIG"]
+trashAccessedAfter = userinfo["trashAfter"]
 
 # GET PATH
 username = getpass.getuser()
@@ -17,7 +33,7 @@ path = "/home/"+username+"/Downloads/*"
 files = []
 for file in glob.glob(path):
     files.append(file)
-print("[i]  Found "+str(len(files))+" Files in Downloads...")
+print("\n[i]  Found "+str(len(files))+" Files in Downloads...")
 
 # CLEAN PATH
 cleanedSth = 0
@@ -27,7 +43,7 @@ for file in files:
     except IOError:
         print("[!]  Cannot get information about :", file.rsplit('/', 1)[-1])
     else:
-        if (time.mktime(time.localtime()) - st[ST_ATIME]) >= trashAccessedAfter:
+        if float(time.mktime(time.localtime()) - st[ST_ATIME]) >= float(trashAccessedAfter):
             send2trash(file)
             print("[i]  Trashed: "+file.rsplit('/', 1)[-1])
             cleanedSth = 1
--- main.py
+++ main.py
@@ -1,17 +1,59 @@
+from configparser import ConfigParser
 import os
 
 clear = lambda: os.system('clear')
 
+#CREATE CONFIG
+config_object = ConfigParser()
+
+config_object["CONFIG"] = {
+    "trashAfter": "",     #in seconds
+}
+
+with open('config.ini', 'w') as conf:
+    config_object.write(conf)
+
+
+# CONFIG
+sel = "100"
+while int(sel) > 5 or int(sel) < 0:
+    clear()
+    print("\n >_  downbloats configuration")
+    sel = input("\n[STEP 1] The clean-up process trashes all files, that have not been accessed for:\n [1] One hour\n [2] Three hours\n [3] One day\n [4] Three days\n [5] One week\n\n Your choice? (1/2/3/4/5) ")
+
 clear()
-print("\n >_  downbloats setup")
+print("\n >_  downbloats configuration")
 
-# CLEANUP SELECTION
-selection = ""
-while selection.lower() != "y" and selection.lower() != "n":
-    selection = input("[?]  Do you want to run the ~/Downlaods/ clean-up? (Y/n)")
+config_object = ConfigParser()
+config_object.read("config.ini")
+userinfo = config_object["CONFIG"]
 
-if selection.lower() == "y":
+if int(sel) == 1:
+    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for one hour.\n")
+    userinfo["trashAfter"] = "3600"
+elif int(sel) == 2:
+    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for three hours.\n")
+    userinfo["trashAfter"] = "10800"
+elif int(sel) == 3:
+    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for one day.\n")
+    userinfo["trashAfter"] = "86400"
+elif int(sel) == 4:
+    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for three days.\n")
+    userinfo["trashAfter"] = "259200"
+elif int(sel) == 5:
+    print("\n[STEP 1]  The clean-up process will trash all files, that have not been accessed for one week.\n")
+    userinfo["trashAfter"] = "604800"
+
+with open('config.ini', 'w') as conf:
+    config_object.write(conf)
+
+
+# CLEANUP sel
+sel = ""
+while sel.lower() != "y" and sel.lower() != "n":
+    sel = input("[?]  Do you want to run the ~/Downlaods/ clean-up? (Y/n)")
+
+if sel.lower() == "y":
     clear()
     os.system('python3 clean.py')
-    clear()
 
